97cca1dcb9643355b5e05a67a486bc00eb2b0e08
Rasesh <raseshpatel9977@gmail.com>
2022-07-14 10:31:10 -0500

    Prior to making changes to Project carousel

--- JavaScript/Dummy.js
+++ JavaScript/Dummy.js
@@ -1,22 +1,8 @@
-// var button = document.getElementsByClassName("");
-var button = document.getElementById("ID-button-div");
-console.log(button);
-console.log("Script has been envoked");
-var image = document.getElementById("ID-image");
-console.log(image);
-// Fetch nextsibling with index as it is also going to be an array
+var buttonMoreItems = document.getElementById("ID-more-items-button");
+console.log(buttonMoreItems);
 
-button.addEventListener("click", function(){
-    if (image.style.display == "block"){
-        image.style.display = "none";
-        button.style.height = "200px";
-        button.style.transition = "0.8s";
-        button.style.backgroundColor = "orange";
-    }
-    else{
-        image.style.display = "block";
-        button.style.height = "50px";
-        button.style.transition = "0.8s";
-        button.style.backgroundColor = "yellow";
-    }
+var additionalProjects = document.getElementById("ID-extended-container");
+buttonMoreItems.addEventListener("click", function(){
+    buttonMoreItems.style.display = "none";
+    additionalProjects.style.display = "flex";
 });
