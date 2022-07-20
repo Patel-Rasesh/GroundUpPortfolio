58d78c3fa766e862b007a38f0994e1609f548a96
Rasesh <raseshpatel9977@gmail.com>
2022-07-20 09:56:04 -0500

    Auto carousel works

--- JavaScript/Carousel-timer.js
+++ JavaScript/Carousel-timer.js
@@ -12,55 +12,65 @@ const positionHorses = (horse, index) => {
     horse.style.left = horseWidth * index + 'px';
 }
 horses.forEach(positionHorses);
+// var amountToMove = 0;
 
 // 0b. Function to move horses in either direction
 const moveHorses= (trackArray, currentHorse, targetHorse) =>{
+    // amountToMove += 496.8;
+    // if (amountToMove > 993.6){
+    //     amountToMove = 0;
+    // }
+    if (targetHorse == null){
+        targetHorse = horses[0];
+    }
     const amountToMove = targetHorse.style.left;
+    var tempAmountToMove = amountToMove+'px';
     trackArray.style.transform = 'translateX(-'+amountToMove+')';
+    // trackArray.style.transform = 'translateX(-'+tempAmountToMove+')';
     currentHorse.classList.remove('current-horse');
     targetHorse.classList.add('current-horse');
+
 }
 
 // 0c. Function to move dots in the nagivation bar
 const moveDots = (currentDot, targetDot) => {
     currentDot.classList.remove('current-horse');
+    if(targetDot == null){
+        targetDot = dots[0];
+    }
     targetDot.classList.add('current-horse');
 }
+
 // 1. Click Next for image on the right
-nextButton.addEventListener('click', event=>{
+nextButton.addEventListener('click', nextClick);
+function nextClick(){
     const currentHorse = trackArray.querySelector('.current-horse');
     const nextHorse = currentHorse.nextElementSibling;
     const currentDot = dotsArray.querySelector('.current-horse');
     const nextDot = currentDot.nextElementSibling;
-    // 1a. Next to the right most horse
-    // if (nextHorse == null){
-    // moveHorses(trackArray, currentHorse, horses[0]);
-    //     // moveDots(currentDot, dotsArray[0]);
-    // }
-    // else{
+
     moveHorses(trackArray, currentHorse, nextHorse);
     moveDots(currentDot, nextDot);
-    //}
-})
+}
+
 // 2. Click Previous for image on the left
-previousButton.addEventListener('click', event => {
+previousButton.addEventListener('click', previousClick);
+
+function previousClick(){
     const currentHorse = trackArray.querySelector('.current-horse');
     const previousHorse = currentHorse.previousElementSibling;
     const currentDot = dotsArray.querySelector('.current-horse');
     const previousDot = currentDot.previousElementSibling;
 
-    // 2a. Previous to the left most horse
-    // if (previousHorse == null){
-    //     moveHorses(trackArray, currentHorse, horses[]);
-    // }
-
     moveHorses(trackArray, currentHorse, previousHorse);
     moveDots(currentDot, previousDot);
-    
-})
+}
 
-// Setting interval for automatic rotation
-// setInterval(nextButton.click(), 5000);
+// Automatic rotation of carousel
+setInterval(function(){
+    nextButton.click();
+}, 3000);
+
 
 // 3. Go to the respective image when clicked on any dot
 dotsArray.addEventListener('click', event=>{
